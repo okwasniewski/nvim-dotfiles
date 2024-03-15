961308d12e2fcabad7507f0aa5908b2d690efb3b
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-03-15 18:12:26 +0100

    feat: tweak git stuff, add diff-view

--- lua/custom/plugins/git-stuff.lua
+++ lua/custom/plugins/git-stuff.lua
@@ -1,3 +1,16 @@
+-- NOTE: Git keymaps
+vim.keymap.set('n', '<leader>gg', function()
+  vim.cmd.LazyGit()
+end, { desc = 'Open LazyGit' })
+
+vim.keymap.set('n', '<leader>gd', function()
+  vim.cmd.DiffviewOpen()
+end, { desc = 'Open diff view' })
+
+vim.keymap.set('n', '<leader>gp', function()
+  vim.cmd.Gitsigns 'preview_hunk'
+end, { desc = 'Preview hunk' })
+
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -12,7 +25,12 @@ return {
     },
     setup = function()
       require('gitsigns').setup()
-      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+    end,
+  },
+  {
+    'sindrets/diffview.nvim',
+    setup = function()
+      require('diffview').setup()
     end,
   },
 }
