2de68c5bbe0f57513f8e835e0ad5a3a53f921197
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-05-24 15:25:07 +0200

    feat: add "o" shortcut to open files in finder

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -15,6 +15,19 @@ return {
           hide_by_name = { '.git', '.DS_Store' },
         },
         follow_current_file = { enabled = true },
+        window = {
+          mappings = {
+            ['o'] = 'system_open',
+          },
+        },
+        commands = {
+          system_open = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            -- macOs: open file in default application in the background.
+            vim.fn.jobstart({ 'open', path }, { detach = true })
+          end,
+        },
       },
     }
 
