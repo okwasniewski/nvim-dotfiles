a02e27be4f4248ac91b65bb5242919b6f9d5bd46
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-07-08 14:53:32 +0200

    feat: change theme to nvim.lua

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,10 +1,20 @@
 return {
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   {
-    'folke/tokyonight.nvim',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'rose-pine'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
