ea45f470d2d9b94f0a2e0846b3ea19b0d4d7ba1c
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-03-23 19:35:01 +0100

    fix: code-folding

--- lua/plugins/code-folding.lua
+++ lua/plugins/code-folding.lua
@@ -7,11 +7,10 @@ return {
       dynamicRegistration = false,
       lineFoldingOnly = true,
     }
-    local language_servers = require('lspconfig').util._available_servers() -- or list servers manually like {'gopls', 'clangd'}
+    local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
     for _, ls in ipairs(language_servers) do
       require('lspconfig')[ls].setup {
         capabilities = capabilities,
-        -- you can add other fields for setting up lsp server in this table
       }
     end
     require('ufo').setup()
