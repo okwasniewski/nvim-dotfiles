a576913c4f87323615563a5ade2ded5dd27d1e64
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-05-07 21:27:04 +0200

    fix: mason breaking change

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -54,8 +54,8 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
+      { 'mason-org/mason.nvim', version = '1.11.0' },
+      { 'mason-org/mason-lspconfig.nvim', version = '1.32.0' },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
