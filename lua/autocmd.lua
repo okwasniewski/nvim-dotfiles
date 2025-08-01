05646cbac29049f163c512e245caf2ecf68fb447
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-08-01 11:50:35 +0200

    feat: reload files when they change on disk

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,3 +1,4 @@
+-- Automatically format on save
 vim.api.nvim_create_autocmd({ 'BufWrite' }, {
   callback = function(args)
     -- Check if EslintFixAll command exists
@@ -9,10 +10,23 @@ vim.api.nvim_create_autocmd({ 'BufWrite' }, {
   end,
 })
 
+-- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
+})
+
+-- Automatically reload files when they change on disk
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+  pattern = '*',
+  callback = function()
+    -- Check if the current buffer is a normal file (not special buffers)
+    if vim.bo.buftype == '' and vim.fn.filereadable(vim.fn.expand '%') == 1 then
+      vim.cmd 'checktime'
+    end
+  end,
+  desc = 'Reload file when terminal gains focus',
 })
